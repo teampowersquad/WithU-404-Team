05c711100420164be2c2b13df5addf00752ded3d
Greg Rentería <gwrenter>
2020-11-21 14:56:51 -0600

    new changes

--- WithU-404Team/htmls/config.php
+++ WithU-404Team/htmls/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "nestedcomment");
+define("DB_SERVER", "fdb27.biz.nf");
+define("DB_USER", "	3659384_nestedcomment");
+define("DB_PASS", "nestedcomment987");
+define("DB_NAME", "3659384_nestedcomment");
 ?>
